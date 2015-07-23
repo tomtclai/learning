136f49f3dffe5c7b61e668f309ffb0f44a6b25e9
Tom Lai <tomtclai@gmail.com>
2015-07-22 18:02:07 -0700

    haskell

--- learn-you-a-haskell/baby.hs
+++ learn-you-a-haskell/baby.hs
@@ -138,3 +138,42 @@ maximum' [] = error "maximum of empty list"
 maximum' [x] = x
 maximum' (x:xs) = max x (maximum' xs)
 
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0   = []
+take' _ []     = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a== x      = True
+    | otherwise  = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort[a | a <- xs, a <= x]
+        biggerSorted  = quicksort[a | a <- xs, a > x]
+    in  smallerSorted ++ [x] ++ biggerSorted
+
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
