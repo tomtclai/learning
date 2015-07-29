c74afed37d54450623f84f315c09f5506c128ba0
Tom Lai <tomtclai@gmail.com>
2015-07-28 22:01:02 -0700

    cat clicker premium

--- Udacity.com/JavaScript-Design-Patterns/catClicker.js
+++ Udacity.com/JavaScript-Design-Patterns/catClicker.js
@@ -1,23 +1,37 @@
 var catNames = ["Tom", "Jerry", "Paul", "Mary", "Hillary Clinton"];
-
-
 var numOfCats = catNames.length;
 var count = [];
 
 $(document).ready(function () {
 
-    for (i in catNames) {
-        $('#catContainer').append(
-            '<div class="catBox" id="catBox' + i + '">\
-            <h1 class="catName" id="catName1">' + catNames[i] + '</h1>\
-            <h1 class="count" id="count' + i + '">0</h1>\
-            <img class="catPic" id="cat' + i + '" src="cat' + i + '.jpg">\
-            </div>'
-        );
+        for (i in catNames) {
+            $('#catList').append(
+                $('<li class="button">').text(catNames[i]).click(makeMainCat(i))
+            );
+            count[i] = 0;
+        }
 
-        count[i] = 0;
+        function makeMainCat(i) {
+            return function () {
+                console.log('show cat ' + i);
 
-        $('#cat' + i).click(createCountIncrement(i));
+                $('#catContainer').html(
+                    '<div class="catBox" id="catBox' + i + '">\
+                    <h2 class="catName" id="catName1">' + catNames[i] + '</h2>\
+                    <h2 class="count" id="count' + i + '">' + count[i] + '</h2>\
+                    <img class="catPic" id="cat' + i + '" src="cat' + i + '.jpg">\
+                    </div>'
+                );
+
+                $('#cat' + i).click(createCountIncrement(i));
+            }
+        }
+
+
+
+
+
+
 
         function createCountIncrement(i) {
             return function countIncrement(e) {
@@ -28,6 +42,14 @@ $(document).ready(function () {
         }
 
 
-    }
-
-})
+    })
+    /*
+    // code for adding all cats to container
+            $('#catContainer').append(
+                '<div class="catBox" id="catBox' + i + '">\
+                <h2 class="catName" id="catName1">' + catNames[i] + '</h2>\
+                <h2 class="count" id="count' + i + '">0</h2>\
+                <img class="catPic" id="cat' + i + '" src="cat' + i + '.jpg">\
+                </div>'
+            );
+    */
