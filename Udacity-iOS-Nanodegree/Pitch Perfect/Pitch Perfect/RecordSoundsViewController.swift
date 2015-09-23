8c38fe3c3bcc393bc9f3073c8de755b502963680
Tom Lai <tomtclai@gmail.com>
2015-09-23 13:14:10 -0700

    readme.. and pics?

---
+++ Udacity-iOS-Nanodegree/Pitch Perfect/Pitch Perfect/RecordSoundsViewController.swift
@@ -0,0 +1,87 @@
+//
+//  RecordSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Tom Lai on 9/23/15.
+//  Copyright © 2015 com.udacity. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    var audioRecorder:AVAudioRecorder!
+    var recordedAudio:RecordedAudio!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+    }
+
+    @IBAction func recordAudio(sender: UIButton) {
+        recordingLabel.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        let recordingName = "audio.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        if(flag){
+        // Save recorded audio
+        recordedAudio = RecordedAudio()
+        recordedAudio.filePathUrl = recorder.url
+        recordedAudio.title = recorder.url.lastPathComponent
+        
+        self.performSegueWithIdentifier("stopRecording", sender: recordedAudio)
+        } else {
+            print ("Recording was not sucessful")
+            recordButton.enabled = true
+            stopButton.hidden = true
+        }
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "stopRecording") {
+            let playSoundsVC:PlaySoundsViewController = segue.destinationViewController as! PlaySoundsViewController
+            let data = sender as! RecordedAudio
+            playSoundsVC.receivedAudio = data
+        }
+    }
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        recordingLabel.hidden = true
+        recordButton.enabled = true
+        stopButton.hidden = true
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+}
+
---
+++ Udacity-iOS-Nanodegree/readme.md
@@ -0,0 +1,5 @@
+Title  | iOS Developer Nanodegree
+-------|-------------------
+Author | UDacity
+Lesson | `4a`
+URL    | [udacity.com](https://www.udacity.com/course/ios-developer-nanodegree--nd003)
