8cb06328f8e117d83f6b7719d18deb6bed374d8d
Tom Lai <tomtclai@gmail.com>
2015-10-20 16:13:51 -0700

    transition animation

---
+++ animationObject/animationObject/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  animationObject
+//
+//  Created by Tom Lai on 10/20/15.
+//  Copyright © 2015 Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let transitionManager = TransitionManager()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction override func unwindForSegue(unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
+        transitionManager.presenting = false
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let toViewController = segue.destinationViewController as UIViewController
+        transitionManager.presenting = true
+        toViewController.transitioningDelegate = self.transitionManager
+    }
+}
+
