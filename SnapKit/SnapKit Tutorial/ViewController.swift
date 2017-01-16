9329deaa533eb2548c31238387c28d4349e3870a
Tom Lai <tomtclai@gmail.com>
2017-01-16 09:57:08 -0800

    add colored UIviews

--- SnapKit/SnapKit Tutorial/ViewController.swift
+++ SnapKit/SnapKit Tutorial/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //1 : Here you just made three views with some background color to better visualise their frames when running the project, and you add them as subviews to the root view.
+        let topView = UIView()
+        topView.backgroundColor = UIColor.blueColor()
+        
+        let bottomLeftView = UIView()
+        bottomLeftView.backgroundColor = UIColor.yellowColor()
+        
+        let bottomRightView = UIView()
+        bottomRightView.backgroundColor = UIColor.grayColor()
+        
+        self.view.addSubview(topView)
+        self.view.addSubview(bottomLeftView)
+        self.view.addSubview(bottomRightView)
+        
+        //2 : Starting by the top view, you basically asked SnapKit to set its top, left and right spacing to 0 so that the view will always stick to the top of the screen and never cross the left and right boundaries of the screen.
+        // Also, you set up an equality relation between the height of the top view and the height of the root view (the screen). This relation imposes that the height of the top view will always be the half of the root view height. This way, you will ensure the top view is always filling exactly 50% of the screen, regardless of the screen size and orientation.
+        topView.snp_makeConstraints { (make) in
+            make.top.left.right.equalTo(0)
+            make.height.equalTo(self.view.snp_height).multipliedBy(0.5)
+        }
+        //3 : For the bottomLeftView, you set its left and bottom spacing constraints to 0, and its height and width to be calculated as the half of the root view. This way, the bottomLeftView will always fill the 25% of the screen located in the bottom left
+        bottomLeftView.snp_makeConstraints { (make) in
+            make.left.bottom.equalTo(0)
+            make.height.equalTo(self.view.snp_height).multipliedBy(0.5) // why not just use * for multiplication?
+            make.width.equalTo(self.view.snp_width).multipliedBy(0.5)
+        }
+        //4 : This is basically the same logic as for the previous bottomLeftView, except that you set the constraints to align it on the bottom right 25% of the screen.
+        bottomRightView.snp_makeConstraints { (make) in
+            make.bottom.right.equalTo(0)
+            make.height.equalTo(self.view.snp_height).multipliedBy(0.5)
+            make.width.equalTo(self.view.snp_width).multipliedBy(0.5)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
