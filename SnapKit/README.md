706cd341b1d9ce4f6f71893ce42a97a71ff3d9d6
Tom Lai <tomtclai@gmail.com>
2017-01-16 09:15:46 -0800

    Init

---
+++ SnapKit/README.md
@@ -0,0 +1,6 @@
+Title  | How to Easily Write Auto Layout Constraints Programmatically with SnapKit
+-------|-------------------
+Author | Malek T.
+URL    | [sweettutos.com](http://sweettutos.com/2015/07/13/how-to-easily-write-auto-layout-constraints-programmatically-with-snapkit/)
+
+
---
+++ SnapKit/SnapKit TutorialTests/SnapKit_TutorialTests.swift
@@ -0,0 +1,36 @@
+//
+//  SnapKit_TutorialTests.swift
+//  SnapKit TutorialTests
+//
+//  Created by Malek T. on 7/9/15.
+//  Copyright (c) 2015 Medigarage Studios LTD. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SnapKit_TutorialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
