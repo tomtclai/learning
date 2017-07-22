3bb7687f4e924854029fd63f5f93a503197bd7bb
Tom Lai <tomtclai@gmail.com>
2017-07-21 18:02:25 -0700

    start ML

---
+++ SceneDetector_Starter/SceneDetector/ViewController.swift
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  // MARK: - IBOutlets
+  @IBOutlet weak var scene: UIImageView!
+  @IBOutlet weak var answerLabel: UILabel!
+
+  // MARK: - Properties
+  let vowels: [Character] = ["a", "e", "i", "o", "u"]
+
+  // MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    guard let image = UIImage(named: "train_night") else {
+      fatalError("no starting image")
+    }
+
+    scene.image = image
+  }
+}
+
+// MARK: - IBActions
+extension ViewController {
+
+  @IBAction func pickImage(_ sender: Any) {
+    let pickerController = UIImagePickerController()
+    pickerController.delegate = self
+    pickerController.sourceType = .savedPhotosAlbum
+    present(pickerController, animated: true)
+  }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate {
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    dismiss(animated: true)
+
+    guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+      fatalError("couldn't load image from Photos")
+    }
+
+    scene.image = image
+  }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension ViewController: UINavigationControllerDelegate {
+}
---
+++ SceneDetector_Starter/readme.md
@@ -0,0 +1,4 @@
+Title  | Core ML and Vision: Machine Learning in iOS 11 Tutorial
+-------|-------------------
+Author | Audrey Tam
+URL    | [raywenderlich.com](https://www.raywenderlich.com/164213/coreml-and-vision-machine-learning-in-ios-11-tutorial)
