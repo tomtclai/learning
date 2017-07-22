7b2763f891796f8d009f25442b917db10c34993f
Tom Lai <tomtclai@gmail.com>
2017-07-22 11:37:32 -0700

    weighted sum

--- Leetcode-swift/My Work.playground/Pages/Nested List Weight Sum.xcplaygroundpage/Contents.swift
+++ Leetcode-swift/My Work.playground/Pages/Nested List Weight Sum.xcplaygroundpage/Contents.swift
@@ -9,9 +9,42 @@
  
  Example 2:
  Given the list `[1,[4,[6]]]`, return 27. (one 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 4*2 + 6*3 = 27)
-
+ 
  
  */
 import Foundation
-
-var str = "Hello, playground"
+// This is the interface that allows for creating nested lists.
+// You should not implement it, or speculate about its implementation
+protocol NestedInteger {
+    // Return true if this NestedInteger holds a single integer, rather than a nested list.
+    func isInteger() -> Bool
+    
+    // Return the single integer that this NestedInteger holds, if it holds a single integer
+    // The result is undefined if this NestedInteger holds a nested list
+    func getInteger() -> Int
+    
+    // Set this NestedInteger to hold a single integer.
+    func setInteger(value: Int)
+    
+    // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+    func add(elem: NestedInteger)
+    
+    // Return the nested list that this NestedInteger holds, if it holds a nested list
+    // The result is undefined if this NestedInteger holds a single integer
+    func getList() -> [NestedInteger]
+}
+class Solution {
+    
+    func depthSum(_ nestedList: [NestedInteger], multiplier: Int = 1) -> Int {
+        var sum = 0
+        for nestedInteger in nestedList {
+            var currentInteger = nestedInteger
+            if nestedInteger.isInteger() {
+                sum += multiplier * nestedInteger.getInteger()
+            } else {
+                sum += depthSum(currentInteger.getList(), multiplier: multiplier + 1)
+            }
+        }
+        return sum
+    }
+}
