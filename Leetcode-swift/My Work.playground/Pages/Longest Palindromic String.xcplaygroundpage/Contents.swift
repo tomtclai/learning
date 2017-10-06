f7813ed5b90cacda04658ce7c57a94784e331c91
Tom Lai <tomtclai@gmail.com>
2017-10-06 11:14:07 -0700

    Swift strings being swift

--- Leetcode-swift/My Work.playground/Pages/Longest Palindromic String.xcplaygroundpage/Contents.swift
+++ Leetcode-swift/My Work.playground/Pages/Longest Palindromic String.xcplaygroundpage/Contents.swift
@@ -25,16 +25,16 @@ func longestPalindrome(_ s: String) -> String {
     // loop that varies the start index O(n)
     // loop that varies the end index O(n)
 
-    var arrayOfInts = [Int]()
-    var tableOfInts = [[Int]]()
+    var arrayBools = [Bool]()
+    var isPalindomeFromTo = [[Bool]]()
     // better solution... remember those smaller solutions
     // make a table of (string size)^2
     s.characters.forEach { _ in
-        arrayOfInts.append(0)
+        arrayBools.append(false)
     }
 
     s.characters.forEach { _ in
-        tableOfInts.append(arrayOfInts)
+        isPalindomeFromTo.append(arrayBools)
     }
 
     // input: babad
@@ -46,6 +46,46 @@ func longestPalindrome(_ s: String) -> String {
     // to    2 T F T
     //       3 F T F T
     //       4 F F F F T
-    return ""
+
+    var maxPalindromeLength = 1
+    var maxPalindromeStart = 0
+    for i in 0..<s.count {
+        // any strings from i to i is 1 character long, 1 character strings are palindromes
+        isPalindomeFromTo[i][i] = true
+    }
+
+    // for max length 2
+    for i in 0..<s.count {
+        if String(s[s.index(s.startIndex, offsetBy: s)]) == String(s[s.index(s.startIndex, offsetBy: s+1)]) {
+            isPalindomeFromTo[i][i+1] = true
+            maxPalindromeStart = i
+            maxPalindromeLength = 2
+        }
+    }
+
+    //for length 3 and above
+    for length in 3...s.count {
+        for start in 0..<s.count-length {
+            let end = start + length - 1
+
+            if isPalindomeFromTo[start+1][end-1] && String(
+                s[s.index(
+                    s.startIndex, offsetBy: start)
+                ]) == String(
+                    s[s.index(
+                        s.startIndex, offsetBy: end)
+                    ])
+            {
+                isPalindomeFromTo[start][end] = true
+                if length > maxPalindromeLength {
+                    maxPalindromeStart = start
+                    maxPalindromeLength = length
+                }
+            }
+        }
+    }
+    let rangeStart = s.index(s.startIndex, offsetBy: maxPalindromeStart)
+    let rangeEnd = s.index(s.startIndex, offsetBy: maxPalindromeStart+maxPalindromeLength)
+    return String(s[rangeStart..<rangeEnd])
 }
 
