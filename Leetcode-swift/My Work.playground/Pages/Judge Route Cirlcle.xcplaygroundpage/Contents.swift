917faa3eda20648ff88a2fb3315bedfe183f83ae
Tom Lai <tomtclai@gmail.com>
2017-10-12 16:00:23 -0700

    add a question

---
+++ Leetcode-swift/My Work.playground/Pages/Judge Route Cirlcle.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/judge-route-circle/description/
+func judgeCircle(_ moves: String) -> Bool {
+    var x = 0
+    var y = 0
+    let charArray = Array(moves.characters)
+    for char in charArray {
+        switch char {
+        case "U":
+            y += 1
+        case "D":
+            y -= 1
+        case "L":
+            x -= 1
+        case "R":
+            x += 1
+        default:
+            print("unexpected characters!!")
+        }
+    }
+    return x==0 && y==0
+}
+
