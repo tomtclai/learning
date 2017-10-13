6bd91ddec44f8c7705a791643878cb0aa5a5b880
Tom Lai <tomtclai@gmail.com>
2017-10-13 12:47:27 -0700

    swift 4 solution

--- Leetcode-swift/My Work.playground/Pages/Reverse String.xcplaygroundpage/Contents.swift
+++ Leetcode-swift/My Work.playground/Pages/Reverse String.xcplaygroundpage/Contents.swift
@@ -1,13 +1,14 @@
   func reverseString(_ s: String) -> String {
-    var scString = s.utf8CString
-    
+    var characters = Array(s.characters)
+
     let length = s.characters.count
     var i = 0
     var j = length - 1
     while i < j {
-        swap(&scString[i], &scString[j])
+        characters.swapAt(i, j)
         i = i + 1
         j = j - 1
     }
-    return String(cString: scString)
+
+    return String(characters)
   }
