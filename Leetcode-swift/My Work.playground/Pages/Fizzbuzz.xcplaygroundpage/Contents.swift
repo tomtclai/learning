43f1a78421df3935cad86fe74840b8c9e7417c6b
Tom Lai <tomtclai@gmail.com>
2017-10-14 11:03:50 -0700

    fizz buzz

---
+++ Leetcode-swift/My Work.playground/Pages/Fizzbuzz.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+
+// https://leetcode.com/problems/fizz-buzz/description/
+func fizzBuzz(_ n: Int) -> [String] {
+    // later optimization, save this outside of the function
+    var resultString = [String]()
+    for i in 1...n {
+        if i % 15 == 0 {
+            resultString.append("FizzBuzz")
+        } else if i % 3 == 0 {
+            resultString.append("Fizz")
+        } else if i % 5 == 0 {
+            resultString.append("Buzz")
+        } else {
+            resultString.append("\(i)")
+        }
+    }
+    return resultString
+}
+
