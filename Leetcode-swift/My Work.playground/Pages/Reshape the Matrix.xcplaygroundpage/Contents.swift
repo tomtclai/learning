b5216563273875be60c847b3276a908bc227b469
Tom Lai <tomtclai@gmail.com>
2017-10-14 12:35:58 -0700

    trim BST

---
+++ Leetcode-swift/My Work.playground/Pages/Reshape the Matrix.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+ // https://leetcode.com/problems/reshape-the-matrix/description/
+  private func dimensionIsValid(rows: Int, cols: Int, forMatrix matrix: [[Int]]) -> Bool {
+    let expectedRows = matrix.count
+    let expectedCols = matrix.first?.count ?? 0
+    return rows * cols == expectedRows * expectedCols
+  }
+  func matrixReshape(_ nums: [[Int]], _ r: Int, _ c: Int) -> [[Int]] {
+    guard dimensionIsValid(rows: r, cols: c, forMatrix: nums) else {
+        return nums
+    }
+    
+    var result = [[Int]]()
+    var flattenedNums = nums.flatMap{$0}
+    for _ in 0..<r {
+        var newRow = [Int]()
+        for _ in 0..<c {
+            newRow.append(flattenedNums.removeFirst())
+        }
+        result.append(newRow)
+    }
+    return result
+  }
---
+++ Leetcode-swift/My Work.playground/Pages/Trim a Binary Search Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+  //https://leetcode.com/problems/trim-a-binary-search-tree/description/
+  func trimBST(_ root: TreeNode?, _ L: Int, _ R: Int) -> TreeNode? {
+    guard var root = root else {
+        return nil
+    }
+    // I'm root and I'm out of range:
+    // return a node that is in range (might be nil)
+    if root.val < L {
+        // trim the left subtree
+        return trimBST(root.right, L, R)
+    } else if root.val > R {
+        // trim the right subtree
+        return trimBST(root.left, L, R)
+    } else {
+        // I'm root and I'm in the range:
+        // recursive calls on each children
+        root.left = trimBST(root.left, L, R)
+        root.right = trimBST(root.right, L, R)
+        return root
+    }
+    
+  }
+  
+  
+  //  Definition for a binary tree node.
+  public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+  }
+  
