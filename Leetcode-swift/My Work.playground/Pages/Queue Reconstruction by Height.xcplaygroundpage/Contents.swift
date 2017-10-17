02561429f645f38eab46ff79ec4adf9232b78c62
Tom Lai <tomtclai@gmail.com>
2017-10-17 12:14:39 -0700

    queue reconstruction by height

---
+++ Leetcode-swift/My Work.playground/Pages/Queue Reconstruction by Height.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+func reconstructQueue(_ people: [[Int]]) -> [[Int]] {
+    var people = people.sorted { a, b in
+        if a[0] > b[0] { return true }
+        else if a[0] < b[0] { return false }
+        else { return a[1] < b[1] }
+    }
+
+    // find where the first number stops
+    guard let firstNumber = people.first?[0] else {
+        return people
+    }
+    guard let index = people.index(where: { return $0[0] != firstNumber}) else {
+        return people
+    }
+
+    var recontructedQueue = Array(people[0..<index])
+
+    for i in index..<people.count {
+        recontructedQueue.insert(people[i], at: people[i][1])
+    }
+
+    return recontructedQueue
+}
+//https://leetcode.com/problems/queue-reconstruction-by-height/description/
