48a61466238f31b9f7a0cea7360cc49056c78139
Tom Lai <tomtclai@gmail.com>
2017-11-08 14:15:25 -0800

    fill in two sum IV

--- Leetcode-swift/My Work.playground/Pages/Two Sum IV - Input is a BST.xcplaygroundpage/Contents.swift
+++ Leetcode-swift/My Work.playground/Pages/Two Sum IV - Input is a BST.xcplaygroundpage/Contents.swift
@@ -1,7 +1,24 @@
-//: [Previous](@previous)
+//https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+var mapForComplements = [Int: Int]()
+func findTarget(_ root: TreeNode?, _ k: Int) -> Bool {
+    guard let root = root else {
+        return false
+    }
+    if mapForComplements.keys.contains(root.val) {
+        return true
+    }
+    mapForComplements[k - root.val] = root.val
+    return findTarget(root.left, k) || findTarget(root.right, k)
+}
 
-import Foundation
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
 
-var str = "Hello, playground"
-
-//: [Next](@next)
