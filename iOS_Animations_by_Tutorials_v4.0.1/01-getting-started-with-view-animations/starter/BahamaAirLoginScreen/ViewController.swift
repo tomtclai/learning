efce66b04ac18eda4ec34491c3dae7e9bff65e6a
Tom Lai <tomtclai@gmail.com>
2017-12-13 19:21:49 -0800

    animate text field

--- iOS_Animations_by_Tutorials_v4.0.1/01-getting-started-with-view-animations/starter/BahamaAirLoginScreen/ViewController.swift
+++ iOS_Animations_by_Tutorials_v4.0.1/01-getting-started-with-view-animations/starter/BahamaAirLoginScreen/ViewController.swift
@@ -78,6 +78,8 @@ class ViewController: UIViewController {
   override func viewWillAppear(_ animated: Bool) {
     [heading, username, password].forEach {$0.center.x -= view.bounds.width}
     super.viewWillAppear(animated)
+    loginButton.center.y += 30.0
+    loginButton.alpha = 0.0
   }
   
   override func viewDidAppear(_ animated: Bool) {
@@ -93,12 +95,27 @@ class ViewController: UIViewController {
         self.password.center.x += self.view.bounds.width
         }, completion: nil
     )
+    UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: {
+        self.loginButton.center.y -= 30.0
+        self.loginButton.alpha = 1.0
+    }, completion: nil)
   }
   
   // MARK: further methods
   
   @IBAction func login() {
     view.endEditing(true)
+    UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options:[], animations: {
+        self.loginButton.bounds.size.width += 80.0
+    }, completion: nil)
+    UIView.animate(withDuration: 0.33, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options:[], animations: {
+        self.loginButton.center.y += 60.0
+        self.loginButton.backgroundColor = UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1)
+        self.spinner.center = CGPoint(x: 40.0,
+                                      y: self.loginButton.frame.size.height/2)
+        self.spinner.alpha = 1
+    }, completion: nil)
+
   }
   
   // MARK: UITextFieldDelegate
