77980a3114dda8f9920abd78978a09f2d0ecff7e
Tom Lai <tomtclai@gmail.com>
2018-01-11 17:48:24 -0400

    starting chapter 1

--- Core-Data-by-Tutorials/01-your-first-core-data-app/starter/HitList/HitList/ViewController.swift
+++ Core-Data-by-Tutorials/01-your-first-core-data-app/starter/HitList/HitList/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
+  var names: [String] = []
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    title = "The List"
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @IBAction func addName(_ sender: UIBarButtonItem) {
+
   }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return names.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = names[indexPath.row]
+    return cell
+  }
+}
