b085de904caeaa50cd8035b19e606436be435ad7
Tom Lai <tomtclai@gmail.com>
2018-01-28 12:46:02 -0400

    add managed class

---
+++ Core-Data-by-Tutorials/02-nsmanagedobject-subclasses/starter/Bow Ties/Bow Ties/Bowtie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by Tom Lai on 1/28/18.
+//  Copyright © 2018 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Bowtie)
+public class Bowtie: NSManagedObject {
+
+}
---
+++ Core-Data-by-Tutorials/02-nsmanagedobject-subclasses/starter/Bow Ties/Bow Ties/Bowtie+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Bowtie+CoreDataProperties.swift
+//  Bow Ties
+//
+//  Created by Tom Lai on 1/28/18.
+//  Copyright © 2018 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Bowtie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Bowtie> {
+        return NSFetchRequest<Bowtie>(entityName: "Bowtie")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var isFavorite: Bool
+    @NSManaged public var lastWorn: NSDate?
+    @NSManaged public var name: String?
+    @NSManaged public var rating: Double
+    @NSManaged public var searchKey: String?
+    @NSManaged public var timesWorn: Int32
+    @NSManaged public var url: URL?
+    @NSManaged public var photoData: NSData?
+    @NSManaged public var tintColor: NSObject?
+
+}
