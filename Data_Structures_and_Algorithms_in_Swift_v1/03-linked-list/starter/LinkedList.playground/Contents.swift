0af246ef8adc5b72d9df8eec49f7655d1c7bb46b
Tom Lai <tomtclai@gmail.com>
2018-07-22 16:49:22 -0300

    push

--- Data_Structures_and_Algorithms_in_Swift_v1/03-linked-list/starter/LinkedList.playground/Contents.swift
+++ Data_Structures_and_Algorithms_in_Swift_v1/03-linked-list/starter/LinkedList.playground/Contents.swift
@@ -1,3 +1,62 @@
 // Copyright (c) 2018 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+public class Node<Value> {
+    public var value: Value
+    public var next: Node?
+
+    public init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+
+extension Node: CustomStringConvertible {
+    public var description: String {
+        guard let next = next else {
+            return "\(value)"
+        }
+        return "\(value) -> " + next.description
+    }
+}
+
+example(of: "Linking notes") {
+    let node1 = Node(value: 1)
+    let node2 = Node(value: 2)
+    let node3 = Node(value: 3)
+
+    node1.next = node2
+    node2.next = node3
+
+    node1
+}
+
+
+public struct LinkedList<Value> {
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+
+    public init() {}
+
+    public var isEmpty: Bool { return head == nil }
+
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        guard let head = head else {
+            return "Empty List"
+        }
+        return String(describing: head)
+    }
+
+    func push() {
+        head = Node(value: Value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    func append()
+    func insert(after: Value)
+}
