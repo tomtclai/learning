c391e245cc5a391a2b61fe2c9b2960e55322214d
Tom Lai <tomtclai@gmail.com>
2018-08-05 14:37:54 -0300

    collection support

--- Data_Structures_and_Algorithms_in_Swift_v1/03-linked-list/starter/LinkedList.playground/Pages/Examples.xcplaygroundpage/Sources/LinkedList.swift
+++ Data_Structures_and_Algorithms_in_Swift_v1/03-linked-list/starter/LinkedList.playground/Pages/Examples.xcplaygroundpage/Sources/LinkedList.swift
@@ -1,12 +1,6 @@
 // Copyright (c) 2018 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
-public func example(of description: String, action: () -> Void) {
-    print("---Example of \(description)---")
-    action()
-    print()
-}
-
 public class Node<Value> {
     public var value: Value
     public var next: Node?
@@ -121,5 +115,41 @@ public struct LinkedList<Value> {
     }
 }
 
+extension LinkedList: Collection {
+    public struct Index: Comparable {
+        public var node: Node<Value>?
 
+        static public func ==(lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+
+            case (nil, nil): return true
+
+            default: return false
+            }
+        }
+
+        static public func <(lhs: Index, rhs: Index) -> Bool {
+            guard lhs != rhs else { return false }
+            let nodes = sequence(first: lhs.node) { $0?.next }
+            return nodes.contains { $0 === rhs.node }
+        }
+    }
+    public var startIndex: Index {
+        return Index(node: head)
+    }
+
+    public var endIndex: Index {
+        return Index(node: tail?.next)
+    }
+
+    public func index(after i: Index) -> Index {
+        return Index(node: i.node?.next)
+    }
+
+    public subscript(position: Index) -> Value {
+        return position.node!.value
+    }
+}
 
