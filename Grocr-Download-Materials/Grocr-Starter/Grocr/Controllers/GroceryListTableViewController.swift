a82865f61804a6e5e6cc6788468b34afe747eade
Tom Lai <tomtclai@gmail.com>
2018-08-31 11:50:20 -0300

    refactor grocery list

--- Grocr-Download-Materials/Grocr-Starter/Grocr/Controllers/GroceryListTableViewController.swift
+++ Grocr-Download-Materials/Grocr-Starter/Grocr/Controllers/GroceryListTableViewController.swift
@@ -31,117 +31,119 @@ import Firebase
 
 class GroceryListTableViewController: UITableViewController {
 
-  // MARK: Constants
-  let listToUsers = "ListToUsers"
-  
-  // MARK: Properties
-  var items: [GroceryItem] = []
-  var user: User!
-  var userCountBarButtonItem: UIBarButtonItem!
-  
-  
-  override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
-  }
-  
-  // MARK: UIViewController Lifecycle
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    tableView.allowsMultipleSelectionDuringEditing = false
-    
-    userCountBarButtonItem = UIBarButtonItem(title: "1",
-                                             style: .plain,
-                                             target: self,
-                                             action: #selector(userCountButtonDidTouch))
-    userCountBarButtonItem.tintColor = UIColor.white
-    navigationItem.leftBarButtonItem = userCountBarButtonItem
-    
-    user = User(uid: "FakeId", email: "hungry@person.food")
-  }
-  
-  // MARK: UITableView Delegate methods
-  
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return items.count
-  }
-  
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
-    let groceryItem = items[indexPath.row]
-    
-    cell.textLabel?.text = groceryItem.name
-    cell.detailTextLabel?.text = groceryItem.addedByUser
-    
-    toggleCellCheckbox(cell, isCompleted: groceryItem.completed)
-    
-    return cell
-  }
-  
-  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-    return true
-  }
-  
-  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-    if editingStyle == .delete {
-      items.remove(at: indexPath.row)
-      tableView.reloadData()
+    // MARK: Constants
+    let listToUsers = "ListToUsers"
+
+    // MARK: Properties
+    var items: [GroceryItem] = []
+    var user: User!
+    var userCountBarButtonItem: UIBarButtonItem!
+    let ref = Database.database().reference(withPath: "grocery-items")
+
+
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
-  }
-  
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    guard let cell = tableView.cellForRow(at: indexPath) else { return }
-    var groceryItem = items[indexPath.row]
-    let toggledCompletion = !groceryItem.completed
-    
-    toggleCellCheckbox(cell, isCompleted: toggledCompletion)
-    groceryItem.completed = toggledCompletion
-    tableView.reloadData()
-  }
-  
-  func toggleCellCheckbox(_ cell: UITableViewCell, isCompleted: Bool) {
-    if !isCompleted {
-      cell.accessoryType = .none
-      cell.textLabel?.textColor = .black
-      cell.detailTextLabel?.textColor = .black
-    } else {
-      cell.accessoryType = .checkmark
-      cell.textLabel?.textColor = .gray
-      cell.detailTextLabel?.textColor = .gray
+
+    // MARK: UIViewController Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.allowsMultipleSelectionDuringEditing = false
+
+        userCountBarButtonItem = UIBarButtonItem(title: "1",
+                                                 style: .plain,
+                                                 target: self,
+                                                 action: #selector(userCountButtonDidTouch))
+        userCountBarButtonItem.tintColor = UIColor.white
+        navigationItem.leftBarButtonItem = userCountBarButtonItem
+
+        user = User(uid: "FakeId", email: "hungry@person.food")
     }
-  }
-  
-  // MARK: Add Item
-  
-  @IBAction func addButtonDidTouch(_ sender: AnyObject) {
-    let alert = UIAlertController(title: "Grocery Item",
-                                  message: "Add an Item",
-                                  preferredStyle: .alert)
-    
-    let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
-      let textField = alert.textFields![0]
-      
-      let groceryItem = GroceryItem(name: textField.text!,
-                                    addedByUser: self.user.email,
-                                    completed: false)
-      
-      self.items.append(groceryItem)
-      self.tableView.reloadData()
+
+    // MARK: UITableView Delegate methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
     }
-    
-    let cancelAction = UIAlertAction(title: "Cancel",
-                                     style: .cancel)
-    
-    alert.addTextField()
-    
-    alert.addAction(saveAction)
-    alert.addAction(cancelAction)
-    
-    present(alert, animated: true, completion: nil)
-  }
-  
-  @objc func userCountButtonDidTouch() {
-    performSegue(withIdentifier: listToUsers, sender: nil)
-  }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        let groceryItem = items[indexPath.row]
+
+        cell.textLabel?.text = groceryItem.name
+        cell.detailTextLabel?.text = groceryItem.addedByUser
+
+        toggleCellCheckbox(cell, isCompleted: groceryItem.completed)
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            items.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        var groceryItem = items[indexPath.row]
+        let toggledCompletion = !groceryItem.completed
+
+        toggleCellCheckbox(cell, isCompleted: toggledCompletion)
+        groceryItem.completed = toggledCompletion
+        tableView.reloadData()
+    }
+
+    func toggleCellCheckbox(_ cell: UITableViewCell, isCompleted: Bool) {
+        if !isCompleted {
+            cell.accessoryType = .none
+            cell.textLabel?.textColor = .black
+            cell.detailTextLabel?.textColor = .black
+        } else {
+            cell.accessoryType = .checkmark
+            cell.textLabel?.textColor = .gray
+            cell.detailTextLabel?.textColor = .gray
+        }
+    }
+
+    // MARK: Add Item
+
+    @IBAction func addButtonDidTouch(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Grocery Item",
+                                      message: "Add an Item",
+                                      preferredStyle: .alert)
+
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            let textField = alert.textFields![0]
+
+            let groceryItem = GroceryItem(name: textField.text!,
+                                          addedByUser: self.user.email,
+                                          completed: false)
+
+            self.items.append(groceryItem)
+            self.tableView.reloadData()
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+
+        alert.addTextField()
+
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+
+        present(alert, animated: true, completion: nil)
+    }
+
+    @objc func userCountButtonDidTouch() {
+        performSegue(withIdentifier: listToUsers, sender: nil)
+    }
 }
