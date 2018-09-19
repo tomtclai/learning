b2a27b8273f7314c07aa405037b208d7959a21ac
Tom Lai <tomtclai@gmail.com>
2018-09-19 12:27:56 -0300

    move challenges

--- Data_Structures_and_Algorithms_in_Swift_v1/04-linked-list-challenge/starter/LinkedListChallenge.playground/Pages/1. Print in reverse.xcplaygroundpage/Contents.swift
+++ Data_Structures_and_Algorithms_in_Swift_v1/04-linked-list-challenge/starter/LinkedListChallenge.playground/Pages/1. Print in reverse.xcplaygroundpage/Contents.swift
@@ -7,7 +7,29 @@
  */
 
 func printInReverse<T>(_ list: LinkedList<T>) {
-  
+  func helper(node: Node<T>?) {
+    // if i am nil
+    //    return
+    guard let node = node else { return }
+    // otherwise
+    //    make recursive call on the next item
+    //    print my value
+    helper(node: node.next)
+    print(node.value)
+  }
+
+  helper(node: list.head)
+}
+
+example(of: "Challenge 1") {
+  var list = LinkedList<Int>()
+  list.push(3)
+  list.push(2)
+  list.push(1)
+
+  print("\(list)")
+
+  printInReverse(list)
 }
 
 //: [Next](@next)
--- Data_Structures_and_Algorithms_in_Swift_v1/04-linked-list-challenge/starter/LinkedListChallenge.playground/Pages/2. Return middle node.xcplaygroundpage/Contents.swift
+++ Data_Structures_and_Algorithms_in_Swift_v1/04-linked-list-challenge/starter/LinkedListChallenge.playground/Pages/2. Return middle node.xcplaygroundpage/Contents.swift
@@ -6,7 +6,25 @@
  */
 
 func getMiddle<T>(_ list: LinkedList<T>) -> Node<T>? {
-  return nil // placeholder 
+  var fast = list.head
+  var slow = list.head
+  while fast != nil {
+    fast = fast?.next?.next
+    slow = slow?.next
+  }
+
+  return slow
 }
 
 //: [Next](@next)
+
+example(of: "Challenge 2") {
+  var list = LinkedList<Int>()
+  list.push(4)
+  list.push(3)
+  list.push(2)
+  list.push(1)
+
+
+  print("middle is \(String(describing: getMiddle(list))) ")
+}
