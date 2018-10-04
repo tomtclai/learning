a844f87c1e47b7daa05de14d1596e6d52d7bed07
Tom Lai <tomtclai@gmail.com>
2018-10-04 18:59:49 -0300

    componentWillMount

--- React/projectmanager/src/App.js
+++ React/projectmanager/src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
 import './App.css';
-
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      projects : []
+    }
+  }
+  
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        title: 'Business website',
+        category: 'Web Design'
+      }, 
+      {
+        title: 'Social App',
+        category: 'Mobile Development'
+      },  
+      {
+        title: 'Ecommerce shopping cart',
+        category: 'Web development'
+      }
+    ]});
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        My app
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
---
+++ React/projectmanager/src/Components/ProjectItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class ProjectItem extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <li className="Project">
+       <strong>{this.props.project.title}</strong> : {this.props.project.category}
+      </li>
+    );
+  }
+}
+
+export default ProjectItem;
