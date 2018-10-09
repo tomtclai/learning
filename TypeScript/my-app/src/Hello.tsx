f9a7ebf4611e5a9fbdfd6fab9262cbff432fdf45
Tom Lai <tomtclai@gmail.com>
2018-10-09 10:09:41 -0300

    start type script

---
+++ TypeScript/my-app/src/Hello.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+export interface Props {
+  name: String;
+  enthusiasmLevel?: number;
+}
+
+function Hello({ name, enthusiasmLevel = 1 }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiastic. :D');
+  }
+
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+    </div>
+  );
+}
+
+export default Hello;
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
