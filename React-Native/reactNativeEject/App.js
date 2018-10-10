47bc946b1980c742c44c78ff160c182933782343
Tom Lai <tomtclai@gmail.com>
2018-10-10 10:27:21 -0300

    image url

--- React-Native/reactNativeEject/App.js
+++ React-Native/reactNativeEject/App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
@@ -14,56 +14,60 @@ import {
   View,
   TextInput,
   Button,
-  SafeAreaView 
-} from 'react-native';
+  SafeAreaView
+} from "react-native";
 
-import ListItem from './src/components/ListItem/ListItem';
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceList from './src/components/PlaceList/PlaceList';
-import placeImage from './src/assets/beautiful-place.jpg';
+import ListItem from "./src/components/ListItem/ListItem";
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+import placeImage from "./src/assets/beautiful-place.jpg";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-
   state = {
     places: []
-  }
+  };
 
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
         places: prevState.places.concat({
-          key: ""+Math.random(), 
+          key: "" + Math.random(),
           value: placeName,
-          image: placeImage
+          image: {
+            uri:
+              "https://web.fdcanet.com/data/files/mall/article/201509071644139049.jpg"
+          }
         })
       };
     });
-  }
+  };
 
   placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place) => {
-          return place.key !== key
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
       };
-    })
-  }
-
+    });
+  };
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-        <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler}/>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
+          places={this.state.places}
+          onItemDeleted={this.placeDeletedHandler}
+        />
       </SafeAreaView>
     );
   }
@@ -72,9 +76,9 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    justifyContent: 'flex-start'
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    justifyContent: "flex-start"
   },
   inputContainer: {
     width: "80%",
