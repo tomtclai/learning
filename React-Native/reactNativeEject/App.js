4d395907655e9ce9f347bcf3669c98bbd613ec9f
Tom Lai <tomtclai@gmail.com>
2018-10-11 18:16:54 -0300

    tab screens with stores

--- React-Native/reactNativeEject/App.js
+++ React-Native/reactNativeEject/App.js
@@ -1,18 +1,29 @@
 import { Navigation } from "react-native-navigation";
-
+import { Provider } from "react-redux";
 import AuthScreen from "./src/screens/Auth/Auth";
 import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
 import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
+import configureStore from "./src/store/configureStore";
 // Register Screens
 
-Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
+const store = configureStore();
+Navigation.registerComponent(
+  "awesome-places.AuthScreen",
+  () => AuthScreen,
+  store,
+  Provider
+);
 Navigation.registerComponent(
   "awesome-places.SharePlaceScreen",
-  () => SharePlaceScreen
+  () => SharePlaceScreen,
+  store,
+  Provider
 );
 Navigation.registerComponent(
   "awesome-places.FindPlaceScreen",
-  () => FindPlaceScreen
+  () => FindPlaceScreen,
+  store,
+  Provider
 );
 
 // Start a App
