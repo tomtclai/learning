5bd45daea80288480ecaae679a80b6a7aaeb5723
Tom Lai <tomtclai@gmail.com>
2018-10-11 14:07:56 -0300

    add debugger

--- React-Native/reactNativeEject/index.js
+++ React-Native/reactNativeEject/index.js
@@ -4,7 +4,6 @@ import React from "react";
 import { AppRegistry } from "react-native";
 import { Provider } from "react-redux";
 import App from "./App";
-import { name as appName } from "./app.json";
 import configureStore from "./src/store/configureStore";
 
 const store = configureStore();
--- React-Native/reactNativeEject/src/store/configureStore.js
+++ React-Native/reactNativeEject/src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, compose } from "redux";
 
 import placesReducer from "./reducers/places";
 
@@ -6,8 +6,14 @@ const rootReducer = combineReducers({
   places: placesReducer
 });
 
+let composeEnhancers = compose;
+
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configureStore = () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, composeEnhancers());
 };
 
 export default configureStore;
