d1ca4b48f0565eda82c889c1b3518966dc93915b
Tom Lai <tomtclai@gmail.com>
2018-12-24 08:29:53 -0400

    add flutter

---
+++ flutter/flutter_app/lib/main.dart
@@ -0,0 +1,20 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+Widget helloRectangle() {
+  return Container(color: Colors.red);
+}
+
+void main() {
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Rectangle'),
+      ),
+      body: helloRectangle(),
+    ),
+  );
+}
