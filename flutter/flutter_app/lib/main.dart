5e668dbc6e2747c4e0022139755951669eae3815
Tom Lai <tomtclai@gmail.com>
2018-12-24 08:45:07 -0400

    the hello app

--- flutter/flutter_app/lib/main.dart
+++ flutter/flutter_app/lib/main.dart
@@ -4,17 +4,34 @@
 
 import 'package:flutter/material.dart';
 
-Widget helloRectangle() {
-  return Container(color: Colors.red);
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.red,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'hello',
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 void main() {
-  MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Hello Rectangle'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Rectangle'),
+        ),
+        body: HelloRectangle(),
       ),
-      body: helloRectangle(),
     ),
   );
 }
