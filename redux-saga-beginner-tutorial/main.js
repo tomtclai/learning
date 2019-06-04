6362da72816f451d9e2c0d78877c3b50b63c5dc7
Tom Lai <tomtclai@gmail.com>
2019-06-04 02:56:22 -0700

    add Xcode 11 project

--- redux-saga-beginner-tutorial/main.js
+++ redux-saga-beginner-tutorial/main.js
@@ -3,11 +3,20 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSafaMiddleware from 'redux-saga'
+
+import { helloSaga } from './sagas'
 
 import Counter from './Counter'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+    reducer,
+    applyMiddleware(sagaMiddleware)
+  )
+
+  sagaMiddleware.run(helloSaga)
 
 const action = type => store.dispatch({type})
 
@@ -16,7 +25,8 @@ function render() {
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+      onDecrement={() => action('DECREMENT')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')} />,
     document.getElementById('root')
   )
 }
