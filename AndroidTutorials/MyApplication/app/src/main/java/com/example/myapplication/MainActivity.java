4e60b3ecbc0177c4dbbd1fe9e0e50d626f71225f
Tom Lai <tomtclai@gmail.com>
2019-09-29 15:13:16 -0700

    Start android tutorial

---
+++ AndroidTutorials/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "Hello World");
+
+    }
+}
