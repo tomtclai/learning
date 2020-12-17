a5fa0919b536a6375ae37828e168ca09be15fc64
Tom Lai <tom@tomtclai.com>
2020-12-16 20:00:28 -0800

    3.4 Managing strings in your app

--- Android-Apprentice/01-getting-set-up-with-android-studio/projects/starter/Timefighter/app/src/main/java/com/example/timefighter/MainActivity.kt
+++ Android-Apprentice/01-getting-set-up-with-android-studio/projects/starter/Timefighter/app/src/main/java/com/example/timefighter/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.timefighter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapMeButton: Button
+    private var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gameScoreTextView = findViewById(R.id.game_score_text_view)
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+        tapMeButton = findViewById(R.id.tap_me_button)
+
+        tapMeButton.setOnClickListener { incrementScore() }
     }
+
+    private fun incrementScore () {
+        score++
+
+        val newScore = "Your Score $score"
+        gameScoreTextView.text = newScore
+    }
+
+    private fun resetGame () {
+
+    }
+
+    private fun startGame () {
+
+    }
+
+    private fun endGame () {
+
+    }
+
 }
