ae1db758c4a438a48229400361108416709b0364
Tom Lai <tom@tomtclai.com>
2021-04-02 16:28:58 -0700

    Chapter 2

--- sui-materials-editions-3.0/02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
+++ sui-materials-editions-3.0/02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
@@ -31,16 +31,67 @@
 /// THE SOFTWARE.
 
 import SwiftUI
-
 struct ContentView: View {
+  @State var game = Game()
+  @State var guess: RGB
+  @State var showScore = false
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    VStack {
+      ColorCircle(color: game.target)
+      if !showScore {
+        Text("R: ??? G: ??? B: ???")
+          .padding()
+      } else {
+        Text(game.target.intString())
+          .padding()
+      }
+      ColorCircle(color: guess)
+      Text(guess.intString())
+        .padding()
+      ColorSlider(value: $guess.red, trackColor: .red)
+      ColorSlider(value: $guess.blue, trackColor: .blue)
+      ColorSlider(value: $guess.green, trackColor: .green)
+      Button("Hit me") {
+        showScore = true
+        game.check(guess: guess)
+      }.alert(isPresented: $showScore) {
+        Alert(
+          title: Text("Your Score"),
+          message: Text(String(game.scoreRound)),
+          dismissButton: .default(Text("OK")) {
+          game.startNewRound()
+          guess = RGB()
+        })
+      }
+    }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    ContentView(guess: RGB())
+  }
+}
+
+struct ColorSlider: View {
+  @Binding var value: Double
+  var trackColor: Color
+  var body: some View {
+    HStack {
+      Text("0")
+      Slider(value: $value)
+        .accentColor(trackColor)
+      Text("255")
+    }
+    .padding(.horizontal)
+  }
+}
+
+struct ColorCircle: View {
+  var color: RGB
+  var body: some View {
+    Circle().fill(
+      Color(rgbStruct: color)
+    )
   }
 }
