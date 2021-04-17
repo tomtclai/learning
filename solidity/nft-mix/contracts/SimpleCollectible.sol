4727a0b9f80d235ccc3c99bf0ed8692a2fd9dc14
Tom Lai <tomtclai@users.noreply.github.com>
2021-04-16 22:38:10 -0700

    nft

--- solidity/nft-mix/contracts/SimpleCollectible.sol
+++ solidity/nft-mix/contracts/SimpleCollectible.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.6;
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectible is ERC721 {
     uint256 public tokenCounter;
-    constructor () public ERC721 ("Dogie", "DOG"){
+    constructor () public ERC721 ("Doggie", "DOG") {
         tokenCounter = 0;
     }
 
     function createCollectible(string memory tokenURI) public returns (uint256) {
         uint256 newItemId = tokenCounter;
+        //creates new nft
         _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        tokenCounter = tokenCounter + 1;
-        return newItemId;
-    }
+        //sender = owner
 
+        // 
+        _setTokenURI(newItemId, tokenURI);
+        // add counter
+        tokenCounter = tokenCounter + 1;
+    }
 }
