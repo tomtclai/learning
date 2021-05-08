1957ccc10c8b466a78736808b0c437aa939e874c
Tom Lai <tomtclai@users.noreply.github.com>
2021-05-08 10:12:18 -0700

    ch10?

--- sui-materials-editions-3.0/10-more-user-input-controls/projects/starter/Kuchi/Shared/SettingsView.swift
+++ sui-materials-editions-3.0/10-more-user-input-controls/projects/starter/Kuchi/Shared/SettingsView.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2020 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,24 +32,44 @@
 
 import SwiftUI
 
-@main
-struct KuchiApp: App {
-//  let userManager = UserManager()
-  
-  init() {
-//    userManager.load()
-  }
-
-  var body: some Scene {
-    WindowGroup {
-      SettingsView()
+struct SettingsView: View {
+  @State var numberOfQuestions = 6
+  @State var learningEnabled: Bool = true
+  @State var dailyReminderEnabled = false
+  @State var dailyReminderTime = Date(timeIntervalSince1970: 0)
+  var body: some View {
+    List {
+      Text("Settings")
+        .font(.largeTitle)
+        .padding(.bottom, 8)
+      Section(header: Text("Appearance")) {}
+      Section(header: Text("Game")) {
+        VStack(alignment: .leading) {
+          Stepper("Number of Questions: \(numberOfQuestions)",
+                  value: $numberOfQuestions,
+                  in: 3...20
+          )
+          Text("Any change will affect the next game")
+            .font(.caption2)
+            .foregroundColor(.secondary)
+        }
+        Toggle("Learning Enabled", isOn: $learningEnabled)
+      }
+      Section(header: Text("Notifications")) {
+        HStack {
+          Toggle("Daily Reminder", isOn: $dailyReminderEnabled)
+          DatePicker("",
+                     selection: $dailyReminderTime,
+                     displayedComponents: .hourAndMinute)
+            .disabled(!dailyReminderEnabled)
+        }
+      }
     }
   }
 }
 
-struct KuchiApp_Previews: PreviewProvider {
-  static var previews: some View {
-//    RegisterView(keyboardHandler: KeyboardFollower())
-    SettingsView()
-  }
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
 }
