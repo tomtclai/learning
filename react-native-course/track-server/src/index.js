0a7d3edbe55f5270c38544559d7c4e4db308873f
Tom <tomtclai@users.noreply.github.com>
2022-01-31 14:28:59 -0800

    track api

--- react-native-course/track-server/src/index.js
+++ react-native-course/track-server/src/index.js
@@ -1,13 +1,16 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth')
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes)
 
 const mongouri = 'mongodb+srv://database:tigris9alloy3foretell4BITING6pibroch3mezzo@cluster0.i7hyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongouri);
