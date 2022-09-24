bd05e926eaa4c016850d25de1c95abc8eb3160e9
Tom Lai <tomtclai@users.noreply.github.com>
2022-09-24 16:07:45 -0700

    Update APIConstants.swift

--- Real-World-iOS-by-Tutorials/01-introduction/projects/starter/PetSave/Core/data/api/APIConstants.swift
+++ Real-World-iOS-by-Tutorials/01-introduction/projects/starter/PetSave/Core/data/api/APIConstants.swift
@@ -33,6 +33,6 @@
 enum APIConstants {
   static let host = "api.petfinder.com"
   static let grantType = "client_credentials"
-  static let clientId = "YourKeyHere"
-  static let clientSecret = "YourSecretHere"
+  static let clientId = "N7aIusyokhaqYpgRMQGigy9p7irKG94W0z9uqrzKdl3SVvSTvE"
+  static let clientSecret = "JtSHicDEgKMl0BZF4dZCL4iZuOdau7FpmjRyGsYs"
 }
