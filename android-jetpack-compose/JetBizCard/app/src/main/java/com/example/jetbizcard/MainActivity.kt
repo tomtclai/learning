4f17d08edd71280c5f5b143f9bc248fed36ff850
Tom Lai <tomtclai@users.noreply.github.com>
2022-09-24 20:19:33 -0700

    card compose

--- android-jetpack-compose/JetBizCard/app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ android-jetpack-compose/JetBizCard/app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CreateBizCard()
                 }
             }
         }
@@ -34,10 +40,27 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun CreateBizCard() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .padding(12.dp),
+            shape = RoundedCornerShape(corner= CornerSize(15.dp)),
+            elevation = 4.dp,
+            backgroundColor = Color.White
+        ) {
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetBizCardTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
