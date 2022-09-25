8e4256713c3c049f010622fff71cb7812a3a46e2
Tom Lai <tomtclai@users.noreply.github.com>
2022-09-24 19:57:47 -0700

    lesson 13 begin

--- android-jetpack-compose/JetBizCard/app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ android-jetpack-compose/JetBizCard/app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.amazon.testapp
+package com.example.jetbizcard
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,13 +10,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.amazon.testapp.ui.theme.TestAppTheme
+import com.example.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TestAppTheme {
+            JetBizCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -37,7 +37,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    TestAppTheme {
-        Greeting("hi hi Android")
+    JetBizCardTheme {
+        Greeting("Android")
     }
 }
---
+++ android-jetpack-compose/TestApp/Test/src/main/java/com/example/test/MyClass.kt
@@ -0,0 +1,5 @@
+package com.example.test
+
+fun main() {
+    print("hello world")
+}
