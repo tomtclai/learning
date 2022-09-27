6a3c8e11b3077faa3250ccefd9c72862e57672ce
Tom Lai <tomtclai@users.noreply.github.com>
2022-09-27 14:48:13 -0700

    Ch 6,, 07:29

--- android-jetpack-compose/JetBizCard/app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ android-jetpack-compose/JetBizCard/app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -3,17 +3,20 @@ package com.example.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
@@ -53,7 +56,34 @@ fun CreateBizCard() {
             elevation = 4.dp,
             backgroundColor = Color.White
         ) {
+            Column(modifier = Modifier
+                .height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                CreateImgeProfile(Modifier)
+            }
+//            Divider()
         }
+    }
+}
+
+@Composable
+private fun CreateImgeProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "Profile image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop,
+        )
     }
 }
 
