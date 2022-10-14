21f65754835fe07f6f9274ed651d3b995e96cd54
Tom Lai <tomtclai@users.noreply.github.com>
2022-10-13 21:34:43 -0700

    add accesstoken manager

--- Real-World-iOS-by-Tutorials/03-data-layer-networking/projects/starter/PetSave/Core/data/api/network/RequestManager.swift
+++ Real-World-iOS-by-Tutorials/03-data-layer-networking/projects/starter/PetSave/Core/data/api/network/RequestManager.swift
@@ -5,9 +5,11 @@ class RequestManager: RequestManagerProtocol {
 
   let apiManager: APIManagerProtocol
   let parser: DataParserProtocol
-  init(apiManager: APIManagerProtocol = APIManager(), parser: DataParserProtocol = DataParser()) {
+  let accessTokenManger: AccessTokenManagerProtocol
+  init(apiManager: APIManagerProtocol = APIManager(), parser: DataParserProtocol = DataParser(), accessTokenManger: AccessTokenManagerProtocol = AccessTokenManager()) {
     self.apiManager = apiManager
     self.parser = parser
+    self.accessTokenManger = accessTokenManger
   }
 
   func perform<T>(_ request: RequestProtocol) async throws -> T where T : Decodable {
@@ -17,9 +19,16 @@ class RequestManager: RequestManagerProtocol {
     return decoded
   }
   func requestAccessToken() async throws -> String {
+
+    if accessTokenManger.isTokenValid() {
+      return accessTokenManger.fetchToken()
+    }
+
     let data = try await apiManager.requestToken()
     let token: APIToken = try parser.parse(data: data)
 
+    try accessTokenManger.refreshWith(apiToken: token)
     return token.bearerAccessToken
+
   }
 }
