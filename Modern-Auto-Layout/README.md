08e4b96cb2c6a738e7f9e3804e1ce9bf7449b49d
Tom Lai <tomtclai@users.noreply.github.com>
2022-11-14 10:27:37 -0800

    add autolayout

---
+++ Modern-Auto-Layout/README.md
@@ -0,0 +1,5 @@
+# Modern Auto Layout
+
+Sample code and solutions for my book on Auto Layout. Find out more about the book here:
+
++ [Modern Auto Layout](https://useyourloaf.com/autolayout/)
--- android-jetpack-compose/introToCompose/app/src/main/java/com/example/introtocompose/MainActivity.kt
+++ android-jetpack-compose/introToCompose/app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.introtocompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +29,70 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntroToComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    var moneyCounter = remember {
+        mutableStateOf(0)
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        color = Color(0xFF546E7A)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "$${moneyCounter.value}", style = TextStyle(
+                color = Color.White,
+                fontSize = 19.sp,
+                fontWeight = FontWeight.ExtraBold
+            ))
+            Spacer(modifier = Modifier.height(100.dp))
+            CreateCircle(moneyCounter.value) {
+                moneyCounter.value = it
+            }
+            if (moneyCounter.value > 25) {
+                Text(text = "Lots of money")
+            }
+        }
+    }
+}
+//@Preview
+@Composable
+fun CreateCircle(moneyCounter: Int = 0, updateMoneyCounter: (Int) -> Unit) {
+
+//    var moneyCounter by remember {
+//        mutableStateOf(0)
+//    }
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(105.dp)
+        .clickable {
+            updateMoneyCounter(moneyCounter + 1)
+            Log.d("Tap", "Create Circle ${moneyCounter}")
+        },
+        shape = CircleShape,
+        elevation = 4.dp
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap ${moneyCounter}")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IntroToComposeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
