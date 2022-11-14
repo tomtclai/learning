1873c50b15ff3bd458cb9fdb0455cd0c95852a88
Tom Lai <tomtclai@users.noreply.github.com>
2022-11-14 15:01:42 -0800

    add templates

---
+++ Modern-Auto-Layout/Xcode-Templates-main/File Templates/Personal/UIKit Class.xctemplate/UIViewControllerSwift/___FILEBASENAME___.swift
@@ -0,0 +1,14 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    private func setupView() {
+
+    }
+}
