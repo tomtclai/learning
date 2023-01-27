db69a1b25135559cbab0c49fe7ca2b92969a8f7b
Tom Lai <tomtclai@users.noreply.github.com>
2023-01-27 10:54:29 -0800

    add more ui for tip

--- android-jetpack-compose/JetTipApp/app/src/main/java/com/example/jettipapp/MainActivity.kt
+++ android-jetpack-compose/JetTipApp/app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.jettipapp
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Input
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jettipapp.components.InputField
 import com.example.jettipapp.ui.theme.JetTipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,14 +33,90 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetTipAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                MyApp {
+                   MainContent()
+
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun MainContent() {
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+
+    ) {
+
+        val totalBillState = remember{
+            mutableStateOf("")
+        }
+        val validState = remember(totalBillState.value) {
+            totalBillState.value.trim().isNotEmpty()
+        }
+        val keyboardController = LocalSoftwareKeyboardController.current
+        Column {
+            TopHeader()
+            InputField(valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    // todo - on value changed
+                    keyboardController?.hide()
+                }
+            )
+            Text(text = "hello...")
+            Text(text = "its me ")
+            Text(text = "i was wondering after all these years...")
+            Text(text = "<i forgot the rest of the lyrics>")
+            Text(text = "hello from the other side")
+        }
+    }
+}
+
+@Composable
+fun TopHeader(totalPerPerson: Double = 0.0) {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
+    , color = Color(0xffE9D7F7)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement =  Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total per person",
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+
+    }
+}
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        content()
     }
 }
 
@@ -33,11 +124,11 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetTipAppTheme {
-        Greeting("Android")
-    }
-}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    MyApp {
+//        Text("hello again2")
+//    }
+//}
---
+++ android-jetpack-compose/JetTipApp/app/src/main/java/com/example/jettipapp/components/Components.kt
@@ -0,0 +1,45 @@
+package com.example.jettipapp.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Money
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+    @Composable
+    fun InputField (
+        modifier: Modifier = Modifier,
+        valueState: MutableState<String>,
+        labelId: String,
+        enabled: Boolean,
+        isSingleLine:Boolean,
+        keyboardType: KeyboardType = KeyboardType.Number,
+        imeAction: ImeAction = ImeAction.Next,
+        onAction: KeyboardActions = KeyboardActions.Default
+    ) {
+        OutlinedTextField(
+            modifier = modifier.padding(bottom = 10.dp, start = 10.dp),
+            value = valueState.value,
+            onValueChange = {valueState.value = it},
+            label = {Text(text = labelId)},
+            leadingIcon = { Icon(imageVector = Icons.Rounded.Money, contentDescription = "Money Icon")},
+            singleLine = isSingleLine,
+            textStyle = TextStyle(fontSize= 18.sp,
+                color = MaterialTheme.colors.onBackground),
+            enabled = enabled,
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+            keyboardActions = onAction
+        )
+    }
