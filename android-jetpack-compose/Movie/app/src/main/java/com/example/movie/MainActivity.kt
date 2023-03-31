3bbcba4f6cf6ba9a13ea04023e93eec358c41261
Tom Lai <tomtclai@users.noreply.github.com>
2023-03-31 10:16:33 -0700

    movie list

--- android-jetpack-compose/Movie/app/src/main/java/com/example/movie/MainActivity.kt
+++ android-jetpack-compose/Movie/app/src/main/java/com/example/movie/MainActivity.kt
@@ -1,43 +1,122 @@
 package com.example.movie
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.movie.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        Scaffold(topBar = {
+            TopAppBar(backgroundColor = Color.Magenta,
+            elevation = 5.dp
+            ) {
+            Text(text = "Movies")
+            }
+        }) {
+         content()
+        }
+    }
+}
+
+@Composable
+fun MainContent(movieList: List<String> = listOf(
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life",
+    "Avatar", "300", "Harry", "Life"
+)) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it) {movie ->
+                    Log.d("MOVIE", "$movie")
+                }
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun MovieRow(movie: String,
+             onItemClick: (String) -> Unit = {}) {
+    Card(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth()
+        .height(130.dp)
+        .clickable {
+            onItemClick(movie)
+        },
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 6.dp
+    ){
+        Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start) {
+            Surface(modifier = Modifier
+                .padding(12.dp)
+                .size(100.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie)
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
+    MyApp {
+        MainContent()
     }
 }
