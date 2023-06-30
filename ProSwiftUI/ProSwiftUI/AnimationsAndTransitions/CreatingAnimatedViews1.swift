81987d4f77108591b1613c66423cac81db929b59
Tom Lai <tomtclai@users.noreply.github.com>
2023-06-30 13:25:52 -0700

    TypingAnimationText

--- ProSwiftUI/ProSwiftUI/AnimationsAndTransitions/CreatingAnimatedViews1.swift
+++ ProSwiftUI/ProSwiftUI/AnimationsAndTransitions/CreatingAnimatedViews1.swift
@@ -21,18 +21,56 @@ extension CreatingAnimatedViews1 {
             Text(value.formatted(.number.precision(.fractionLength(fractionLength))))
         }
     }
+
+    struct TypingAnimationText: View, Animatable {
+        @Environment(\.accessibilityVoiceOverEnabled) var accessbilityVoiceOverEnabled
+
+        var string: String
+        var count = 0
+
+        var animatableData: Double{
+            get { Double(count) }
+            set { count = Int(max(0, newValue)) }
+        }
+        var body: some View {
+            if accessbilityVoiceOverEnabled {
+                Text(string)
+            } else {
+                ZStack{
+                    Text(string.prefix(count))
+                    Text(string.prefix(count)).hidden(true)
+                }
+            }
+        }
+    }
 }
 
 struct CreatingAnimatedViews1: SelfCreatingView {
-    @State private var value = 0.0
+
+    @State private var value = 0
+    let message = "hello world"
 
     var body: some View {
-        CountingText(value: value)
-            .onTapGesture {
+        VStack{
+            //        CountingText(value: value)
+            //            .onTapGesture {
+            //                withAnimation(.linear) {
+            //                    value = Double.random(in: 1...1000)
+            //                }
+            //            }
+
+            TypingAnimationText(string: message, count: value).frame(width: 300, alignment:  .leading)
+
+            Button("Type") {
                 withAnimation(.linear) {
-                    value = Double.random(in: 1...1000)
+                    value = message.count
                 }
             }
+
+            Button("Reset") {
+                value = 0
+            }
+        }
     }
 }
 
