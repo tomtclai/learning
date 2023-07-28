0997ef3ee3b999a72780d418be83397b7e92d0e8
Tom Lai <tomtclai@users.noreply.github.com>
2023-07-28 10:35:11 -0700

    add project code…

---
+++ SwiftUI-By-Example/iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Lai, Tom on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
