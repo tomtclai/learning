17558da8d9a7a9cb930de2974cb8bbf044fa9f99
Tom Lai <tomtclai@users.noreply.github.com>
2025-04-13 14:14:06 -0700

    Text

---
+++ SwiftUI-By-Example/iDine/iDine/TextTestingView.swift
@@ -0,0 +1,81 @@
+//
+//  TextTestingView.swift
+//  Chapter 3 Static Text
+
+//  Created by Tom Lai on 4/13/25.
+//
+
+
+import SwiftUI
+
+struct TextTestingView: View {
+    var message1: AttributedString {
+        var result = AttributedString("Hello")
+        result.font = .largeTitle
+        result.foregroundColor = .white
+        result.backgroundColor = .red
+        result.underlineStyle = Text.LineStyle(pattern: .solid, color: .white)
+        return result
+    }
+    var message2: AttributedString {
+        let string = " world world world .... . . . . . . "
+        var result = AttributedString()
+        
+        for (index, letter) in string.enumerated() {
+            var letterString = AttributedString(String(letter))
+            letterString.baselineOffset = sin(Double(index))*5
+            result += letterString
+        }
+        result.font = .largeTitle
+        result.foregroundColor = .blue
+        result.backgroundColor = .green
+        result.underlineStyle = Text.LineStyle(pattern: .dot, color: .white)
+        
+        return result
+    }
+    
+    var link: AttributedString {
+        var result = AttributedString("Learn swift here")
+        result.font = .largeTitle
+        result.link = URL(string: "https://www.hackingwithswift.com")
+        return result
+    }
+    
+    var date: AttributedString {
+        var result = Date.now.formatted(.dateTime.weekday(.wide).day().month(.wide).attributedStyle)
+        result.foregroundColor = .secondary
+        
+        let weekday = AttributeContainer.dateField(.weekday)
+        let weekdayStyling = AttributeContainer.foregroundColor(.primary)
+        result.replaceAttributes(weekday, with: weekdayStyling)
+        return result
+    }
+    var body: some View {
+        VStack{
+     
+            Text(message1+message2)
+                .background(.yellow)
+                // Attributed String stays attributed, modifiers dont work here.
+            Text("This is shoter text.")
+                .lineLimit(6, reservesSpace: true)
+                .frame(width: 200)
+            Text("This is some longer text that is limited to three lines maximum, so anything more than that will cause the text to clip. longer longer longer too long to fit")
+                .lineLimit(10, reservesSpace: true)
+                .padding()
+                .fontDesign(.none)
+                .fontWidth(.condensed) // fontWidth only works if font supports it.
+                .lineSpacing(10)
+                .font(.headline)
+                .background(.gray)
+                .foregroundStyle(.white.gradient)
+                .frame(width: 200)
+            Text(link)
+            Text(date)
+        }
+            
+    }
+}
+
+#Preview {
+    TextTestingView()
+}
