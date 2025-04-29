4eb18501ef6a6215006a55293d33ecb05766329e
Tom Lai <tomtclai@users.noreply.github.com>
2025-04-29 08:13:25 -0700

    gesture assignment

--- UdacitySwiftUIDataLessons/lesson-3/concept2-Photo-Viewer/starter/PhotoViewer/ContentView.swift
+++ UdacitySwiftUIDataLessons/lesson-3/concept2-Photo-Viewer/starter/PhotoViewer/ContentView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    @GestureState var currenOffset = CGSize.zero
+    @State var offset = CGSize.zero
+    @GestureState var currentAngle = Angle.zero
+    @State var angle = Angle.zero
+    @GestureState var currentScale : CGFloat = 1.0
+    @State var scale : CGFloat = 1.0
     var body: some View {
         VStack {
             Image(.cliffside)
                 .resizable()
                 .scaledToFit()
                 .frame(width: 300)
-                // add gestures here
+                .offset(x: currenOffset.width + offset.width,
+                        y: currenOffset.height + offset.height)
+                .simultaneousGesture(dragGesture)
+                .scaleEffect(currentScale * scale)
+                .simultaneousGesture(zoomGesture)
+                .rotationEffect(currentAngle + angle)
+                .simultaneousGesture(rotateGesture)
+            
         }
+    }
+    
+    var dragGesture: some Gesture {
+        DragGesture()
+            .updating($currenOffset) { value, state, _ in
+                state = value.translation
+            }
+            .onEnded { value in
+                offset = CGSize(width: offset.width + value.translation.width,
+                                height: offset.height + value.translation.height)
+            }
+    }
+    
+    var rotateGesture: some Gesture {
+        RotateGesture()
+            .updating($currentAngle) { value, state, _ in
+                state = value.rotation
+            }
+            .onEnded { value in
+                angle = angle + value.rotation
+            }
+    }
+    var zoomGesture: some Gesture {
+        MagnifyGesture()
+            .updating($currentScale) { value, state, _ in
+                state = value.magnification
+            }
+            .onEnded { value in
+                scale *= value.magnification
+            }
     }
 }
 
