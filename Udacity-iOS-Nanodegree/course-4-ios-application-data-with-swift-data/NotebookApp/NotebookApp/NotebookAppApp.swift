8d997f3f1f5e470ee03ff22698afa6ed345fa6c7
Tom Lai <tomtclai@users.noreply.github.com>
2025-05-10 15:05:36 -0700

    organizing folders

---
+++ Udacity-iOS-Nanodegree/course-4-ios-application-data-with-swift-data/NotebookApp/NotebookApp/NotebookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotebookAppApp.swift
+//  NotebookApp
+//
+//  Created by Tom Lai on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NotebookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
